4f73ae4c46e1b44f3521462a0e079ed8086ad758
Nischay Gupta <79783629+NickDL29@users.noreply.github.com>
2022-10-10 06:27:01 +0530

    Update 15.2.c

--- 15.2.c
+++ 15.2.c
@@ -1,42 +1,50 @@
-/* A naive recursive implementation that simply
-follows the above optimal substructure property */
+// See the Cormen book for details of the following algorithm
 #include <limits.h>
 #include <stdio.h>
 
 // Matrix Ai has dimension p[i-1] x p[i] for i = 1..n
-int MatrixChainOrder(int p[], int i, int j)
+int MatrixChainOrder(int p[], int n)
 {
-	if (i == j)
-		return 0;
-	int k;
-	int min = INT_MAX;
-	int count;
 
-	// place parenthesis at different places between first
-	// and last matrix, recursively calculate count of
-	// multiplications for each parenthesis placement and
-	// return the minimum count
-	for (k = i; k < j; k++) {
-		count = MatrixChainOrder(p, i, k) +
-				MatrixChainOrder(p, k + 1, j) +
-				p[i - 1] * p[k] * p[j];
+	/* For simplicity of the program, one extra row and one
+	extra column are allocated in m[][]. 0th row and 0th
+	column of m[][] are not used */
+	int m[n][n];
 
-		if (count < min)
-			min = count;
+	int i, j, k, L, q;
+
+	/* m[i, j] = Minimum number of scalar multiplications needed
+	to compute the matrix A[i]A[i+1]...A[j] = A[i..j] where
+	dimension of A[i] is p[i-1] x p[i] */
+
+	// cost is zero when multiplying one matrix.
+	for (i = 1; i < n; i++)
+		m[i][i] = 0;
+
+	// L is chain length.
+	for (L = 2; L < n; L++) {
+		for (i = 1; i < n - L + 1; i++) {
+			j = i + L - 1;
+			m[i][j] = INT_MAX;
+			for (k = i; k <= j - 1; k++) {
+				// q = cost/scalar multiplications
+				q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+				if (q < m[i][j])
+					m[i][j] = q;
+			}
+		}
 	}
 
-	// Return minimum count
-	return min;
+	return m[1][n - 1];
 }
 
-// Driver program to test above function
 int main()
 {
-	int arr[] = { 30,1,40,10,25 };
-	int n = sizeof(arr) / sizeof(arr[0]);
+	int arr[] = { 1, 2, 3, 4 };
+	int size = sizeof(arr) / sizeof(arr[0]);
 
 	printf("Minimum number of multiplications is %d ",
-		MatrixChainOrder(arr, 1, n - 1));
+		MatrixChainOrder(arr, size));
 
 	getchar();
 	return 0;
